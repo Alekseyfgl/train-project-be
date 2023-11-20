f33065be94356edfffc02d4c2beeb6a74057af3c
aleksey <aleksey.fgl@gmail.com>
2023-11-20 19:14:28 +0000

    fix validations

--- src/server/express/routes/post.router.ts
+++ src/server/express/routes/post.router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { authMiddleware } from '../common/middlewares/auth/auth.middleware';
-import { blogValidation } from '../common/express-validators/blog.validator';
 import { postController } from '../controllers/post.controller';
+import { postValidation } from '../common/express-validators/post.validator';
 
 export const postPath = {
     base: '/post',
@@ -11,6 +11,6 @@ const { base, id } = postPath;
 export const postRouter = Router({});
 postRouter.get(`${base}`, postController.getAll);
 postRouter.get(`${base}/${id}`, postController.getById);
-postRouter.post(`${base}`, authMiddleware, blogValidation(), postController.addByOne);
-postRouter.put(`${base}/${id}`, authMiddleware, blogValidation(), postController.updateById);
+postRouter.post(`${base}`, authMiddleware, postValidation(), postController.addByOne);
+postRouter.put(`${base}/${id}`, authMiddleware, postValidation(), postController.updateById);
 postRouter.delete(`${base}/${id}`, authMiddleware, postController.removeById);
