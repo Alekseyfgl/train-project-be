2f6ec13125a2202de5603d478c977ad10e087680
aleksey <aleksey.fgl@gmail.com>
2024-01-28 16:45:12 +0000

    add repositories for working with devices

--- src/server/express/repositories/device-session/query-device-session.repository.ts
+++ src/server/express/repositories/device-session/query-device-session.repository.ts
@@ -1,1 +1,14 @@
-export class QueryDeviceSessionRequestRepository {}
+import { PromiseNull } from '../../common/interfaces/optional.types';
+import { DeviceSessionModel } from '../../models/device-session.model';
+import { IDeviceSessionSchema } from '../../types/device-session/output';
+
+export class QueryDeviceSessionRequestRepository {
+    static async findByDeviceId(deviceId: string): PromiseNull<IDeviceSessionSchema> {
+        try {
+            return await DeviceSessionModel.findOne({ deviceId });
+        } catch (e) {
+            console.log('QueryDeviceSessionRequestRepository [findByDeviceId]', e);
+            return null;
+        }
+    }
+}
