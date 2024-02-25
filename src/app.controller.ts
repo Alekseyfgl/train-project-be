271635e443804983694739582e249d2db87ce000
aleksey <aleksey.fgl@gmail.com>
2024-02-25 14:24:18 +0000

    add mongo db

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatsRepository } from './cats/cat.repository';
+import { CatDocument } from './cats/cat.schema';
 
-@Controller()
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catRepository: CatsRepository,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('cats')
+  getCats() {
+    return this.catRepository.findAll();
+  }
+
+  @Post('cats')
+  createCat(@Body() dto: any) {
+    return this.catRepository.create(dto);
+  }
+
+  @Put('cats')
+  async updateCat(@Param('id') id: any) {
+    const targetCat: CatDocument = (await this.catRepository.findAll())[0];
+    targetCat.setAge(30);
+    targetCat.save();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserRepository } from './user/user.repository';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cats/cat.schema';
+import { CatsRepository } from './cats/cat.repository';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/train_project_be_local'),
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+  ],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService, UserRepository],
+  providers: [AppService, UserService, UserRepository, CatsRepository],
 })
 export class AppModule {}
